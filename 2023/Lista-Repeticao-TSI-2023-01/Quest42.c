5c80529adb0355b8f208395373eb8c6d490a1653
Oenghus-Gellius <bolivar.augustus@gmail.com>
2023-04-17 20:54:31 -0300

    17/04

--- 2023/Lista-Repeticao-TSI-2023-01/Quest42.c
+++ 2023/Lista-Repeticao-TSI-2023-01/Quest42.c
@@ -8,7 +8,7 @@ cos( X ) = 1 - X^2/2! + X^4/4! - X^6/6! + X^8/8! ….
 
 int main(){
     int x,i,sinal,count;
-    double cos=1,numerador=1,denominador=1;
+    double cos=1,numerador=1,denominador=1,expoente=0;
     const int QTD = 15;
 
     printf("Informe o valor de 'x'.:");
@@ -19,7 +19,7 @@ int main(){
 
     for (i = 0; i < QTD; i++)
     {
-        numerador=sinal*pow(x,i);
+        numerador=sinal*pow(x,expoente);
 
         for (count = 1; count <= i; count++)
         {
@@ -28,6 +28,8 @@ int main(){
         cos=cos+(numerador/denominador);
 
         sinal=sinal*-1;
+        expoente=expoente+2;
+
     }
     printf("\no cos(%d) = %2lf",x,cos);
     
